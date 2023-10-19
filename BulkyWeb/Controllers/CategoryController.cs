edd86c028a39b76d9eb231e6a27b55a7fdfc9af0
Julia <ogorodnicukulia@gmail.com>
2023-10-20 02:22:49 +0300

    Get and post action for delete category delete category in action

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -68,5 +68,33 @@ namespace BulkyWeb.Controllers
             return View();
         }
 
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? category = _db.Categories.Find(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category category = _db.Categories.Find(id);
+            if(category == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(category);
+            _db.SaveChanges();
+            return RedirectToAction("Index", "Category");
+        }
+
     }
 }
