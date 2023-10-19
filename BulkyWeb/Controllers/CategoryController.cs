7210b43ab3dac2705adb7180c4875223162eb1dc
Julia <ogorodnicukulia@gmail.com>
2023-10-20 02:14:58 +0300

    Edit and delete buttons Get category detauls to edit Update category Update category in action

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -31,10 +31,6 @@ namespace BulkyWeb.Controllers
                 ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
             }
 
-            //if (category.Name != null && category.Name.ToLower() == "test")
-            //{
-            //    ModelState.AddModelError("", "Test is an invalid value");
-            //}
             if (ModelState.IsValid)
             {
                 _db.Categories.Add(category);
@@ -43,5 +39,34 @@ namespace BulkyWeb.Controllers
             }
             return View();
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? category = _db.Categories.Find(id);
+            //Category? category1 = _db.Categories.FirstOrDefault(c=> c.Id == id);
+            //Category? category2 = _db.Categories.Where(c=> c.Id == id).FirstOrDefault();
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+        }
+
     }
 }
