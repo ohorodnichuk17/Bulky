520ce7c1f0b8d3fd0bb64ff2e4b26ed7a074ce6e
Julia <ogorodnicukulia@gmail.com>
2023-11-18 02:29:07 +0200

    Add helper methods in order header repository

---
+++ Bulky.DataAccess/Migrations/20231118001328_addSessionIdToOrderHeader.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bulky.DataAccess.Migrations
+{
+    public partial class addSessionIdToOrderHeader : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SessionId",
+                table: "OrderHeaders",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SessionId",
+                table: "OrderHeaders");
+        }
+    }
+}
